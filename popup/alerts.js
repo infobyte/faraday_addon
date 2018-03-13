c30874ca3085e5fa84e16265670f16a9022b9c7d
Federico Fernandez <fedef@infobytesec.com>
2018-03-13 19:22:22 -0300

    Alertas y mensajes de error

---
+++ popup/alerts.js
@@ -0,0 +1,8 @@
+var alerts = {'noWorkspaces': '<div id="message" class="alert alert-danger">No workspaces were found on the Server.</div>',
+			  'errorConnectionServer': '<div id="message" class="alert alert-danger">An error occurred connecting to the server.</div>',
+			  'internalErrorServer': '<div class="alert alert-danger"><strong>ERROR!</strong>An internal error occurred on the server, make sure you have logged in or you are still active.</div>',
+			  'messageAddVuln': '<div class="alert alert-success"><strong>Success!</strong>The vulnerability has been added to Faraday.</div>',
+			  'noConfigData': 'Make sure you have configured the addon correctly.',
+			  'workspaceNoExist': 'The workspace does not exist.',
+			  'internalErrorServer2': 'An internal error occurred on the server, make sure you have logged in or you are still active.',
+			  'noRequestsFound': '<div class="alert alert-info">No requests were found.</div>'};
