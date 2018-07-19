2b1452408eaf06383a7ce1185f7e04f507ea8276
q3rv0 <fedef@infobytesec.com>
2018-07-19 15:08:15 -0300

    option added capture faraday server

--- background.js
+++ background.js
@@ -4,6 +4,8 @@ var requests = {};
 var tabId = 0;
 var requests_tmp = {};
 var power = false;
+var ifserver = false;
+var faraday_server = "";
 
 
 browser.storage.onChanged.addListener(logStorageChange);
@@ -24,7 +26,7 @@ function returnRequests() {
 }
 
 function getRequestAndResponseBody(e) {
- if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power){ // Verifico que el tabId dle request sea igual al tabId de donde estamos parados
+ if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power || (ifserver && getDomainByUrl(e.url) == getDomainByUrl(faraday_server))){ // Verifico que el tabId dle request sea igual al tabId de donde estamos parados
  	requests_tmp = {'method': e.method, 'url': e.url,'requestBody': e }; //Creamos un array dentro del array requests, que tiene como nombre el requestId
  	console.log(e);
 
@@ -51,7 +53,7 @@ function getRequestAndResponseBody(e) {
 }
 
 function getResquestHeaders(e){
-	if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power){ // Verifico que el tabId del request sea igual al tabId de donde estamos parados
+	if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power || (ifserver && getDomainByUrl(e.url) == getDomainByUrl(faraday_server))){ // Verifico que el tabId del request sea igual al tabId de donde estamos parados
 		try{  //Esto lo hago por que solo estoy creando un array a partir del main_frame, es decir el primer request
 			requests_tmp['requestHeaders'] = e.requestHeaders; //Guardamos el request headers en el array requests, en su correspondiente requestId
 			console.log(e);
@@ -63,7 +65,7 @@ function getResquestHeaders(e){
 }
 
 function getResponseHeaders(e){
-	if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power){ // Verifico que el tabId dle request sea igual al tabId de donde estamos parados
+	if(tabId == e.tabId && getDomainByUrl(e.url).match(target) != null && power || (ifserver && getDomainByUrl(e.url) == getDomainByUrl(faraday_server))){ // Verifico que el tabId dle request sea igual al tabId de donde estamos parados
 		try{  //Esto lo hago por que solo estoy creando un array a partir del main_frame, es decir el primer request
 			requests_tmp['responseHeaders'] = e.responseHeaders; //Guardamos el response headers en el array requests, en su correspondiente requestId
 			requests_tmp['statusLine'] = e.statusLine; //get status line
